3b37f295424c048524de5ec29b3f7d368a84dac3
Sonya2114 <stulova.2004@mail.ru>
2022-10-23 00:14:45 +0700

    Error email

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ApiService} from '../services/api.service';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
@@ -12,6 +12,9 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 export class DialogComponent implements OnInit {
   personForm !: FormGroup;
   actionBtn: string = 'Save'
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
 
   constructor(private formBuilder: FormBuilder,
               private api: ApiService,
